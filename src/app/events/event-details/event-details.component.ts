6c7e5fdb71796c5b8d1f59635fc5645645414e7c
Regan Beukes <regan.beukes@pamgolding.co.za>
2018-08-03 12:39:02 +0200

    end of module 6

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -3,7 +3,7 @@ import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    templateUrl: './event-details.component.html'
+    templateUrl: './event-details.component.html',
     styles: [`
     .container {padding-left: 20px; padding right: 20px;}
     .event-image {height: 100px;}    
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -20,7 +20,7 @@ import { ActivatedRoute } from '@angular/router';
 
 export class EventsListComponent implements OnInit {
     events: any
-    constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute){
+    constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute){
         
     }
 
